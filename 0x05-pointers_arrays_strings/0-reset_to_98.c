ad7924da574b86bfafd0613e81eca1b9f2247de5
roselineuduk <rozayjoe247@gmail.com>
2022-09-19 16:51:33 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,47 +1,31 @@
-mkdir 0x05-pointers_arrays_strings
+0-reset_to_98.c
 
-cd 0x05-pointers_arrays_strings
-
-echo pointers arrays and strings > README.md
-
-add the _putchar.c
-
-main.h
-
-#ifndef MAIN_H
-
-#define MAIN_H
-
-
-
-#include <stdio.h>
-
-
-
-int _putchar(char c);
-
-void reset_to_98(int *n);
-
-void swap_int(int *a, int *b);
-
-int _strlen(char *s);
-
-void _puts(char *str);
-
-void print_rev(char *s);
-
-void rev_string(char *s);
-
-void puts2(char *str);
-
-void puts_half(char *str);
-
-void print_array(int *a, int n);
-
-char *_strcpy(char *dest, char *src);
-
-int _atoi(char *s);
+/**
+ *
+ *  * reset_to_98 - takes a pointer to an int parameter
+ *
+ *   *               and updates the value it points to 98
+ *
+ *    *
+ *
+ *     * @n: int parameter
+ *
+ *      *
+ *
+ *       * Return: Nothing
+ *
+ *        */
 
 
 
-#endif /* MAIN_H */
+void reset_to_98(int *n)
+
+{
+
+	        *n = 98;
+
+}
+
+
+
+
